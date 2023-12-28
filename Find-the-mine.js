ee18f44f35608173a20ee251b01bd49d37838234
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-28 11:16:55 -0800

    Update Find-the-mine.js

--- Find-the-mine.js
+++ Find-the-mine.js
@@ -22,3 +22,13 @@ Examples:
 
 
 // Solution
+
+function mineLocation(field){
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      if (field[i][j] === 1) {
+        return [i, j] 
+      }
+    }
+  }
+}
